2e90f1a092aaab530ac5a057c19ea79fd0a1af21
DutaAndrei1 <44239501+DutaAndrei1@users.noreply.github.com>
2018-11-01 11:24:14 +0200

    Add files via upload

---
+++ Ex1.m
@@ -0,0 +1,9 @@
+a=0:0.1:2;
+b=linspace(1,1,21);% a)Matricea b trebuie sa aiba aceeasi lungime ca a.
+b(:)
+b=ans;
+a*b %inmultire a cu b
+b*a % b) inmutire b cu a
+a(:)
+a=ans;
+a.*b %c) inmultire a cu b element cu element
