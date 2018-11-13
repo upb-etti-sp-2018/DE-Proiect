bb2dd6da6ceda676b4980254d2611f140ea85b09
DutaAndrei1 <44239501+DutaAndrei1@users.noreply.github.com>
2018-11-13 18:36:02 +0200

    Add files via upload

---
+++ Ex3.m
@@ -0,0 +1,4 @@
+a=randn(1,10)
+b=randn(1,10)
+z=complex(a,b)
+functie(a,b,z);
---
+++ functie.m
@@ -0,0 +1,6 @@
+function[ma,patrat,inmultire]=fct(a,b,z)
+ma=mean(a)
+patrat=z.^2
+transpusa=z.'
+inmultire=z*transpusa
+end
